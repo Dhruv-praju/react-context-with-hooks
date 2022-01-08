2d22fee2ded3521508c53c5ee45ab1100d5d7a18
dhruv <prajapati.dp@somaiya.edu>
2022-01-08 12:28:07 +0530

    made navbar

---
+++ src/styles/NavbarStyles.js
@@ -0,0 +1,13 @@
+const navStyles = {
+    appbar:{
+        bgcolor:'primary.dark',
+    },
+    switch:{
+        color:'red'
+    },
+    title:{
+        // hide in small view
+        display:{'xs':'none', 'sm':'block'}
+    }
+}
+export default navStyles
